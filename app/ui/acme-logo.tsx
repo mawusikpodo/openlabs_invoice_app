99a029e1ae9604397b0a4a5301fd88fdfe6b59ed
Mawusi Kpodo <mawusikpodo@gmail.com>
2024-07-24 18:32:28 +0000

    initial commit

---
+++ app/ui/acme-logo.tsx
@@ -0,0 +1,21 @@
+// import {ArrowDownOnSquareIcon  } from '@heroicons/react/24/outline';
+import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
+
+export default function Logo() {
+  return (
+    <div
+      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+    >
+      {/* <ArrowDownOnSquareIcon className="h-12 w-12 rotate-[15deg]" /> */}
+      <Image
+            src="/logo.svg"
+            width={500}
+            height={500}
+            className="hidden md:block"
+            alt="Logo of Openlabs"
+          />
+      {/* <p className="text-[44px]">OpenLabs</p> */}
+    </div>
+  );
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana,  Playwrite_AU_NSW } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({weight: ['400', '700'], subsets: ['latin'] });
+
+export const playwrite = Playwrite_AU_NSW({weight: ['400', '100'], });
+
